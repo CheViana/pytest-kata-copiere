c8dd5708ff6860a7bcab5cb5226322713180a8f5
Radetska, Ievgeniia <ievgeniia.radetska@nih.gov>
2023-04-26 17:15:50 -0400

    unsolved version

--- tests/test_copiere.py
+++ tests/test_copiere.py
@@ -11,41 +11,29 @@ from unittest.mock import Mock, patch, call, ANY
 from copiere.copy import copy_file_or_dir
 
 
-@patch("copiere.copy.shutil")
-def test_copiere_copies_file(shutil_mock):
+# patch shutil
+def test_copiere_copies_file():
     # Arrange
-    file_mock = Mock()
-    file_mock.is_dir.return_value = False
-    file_mock.name = 'myfile.txt'
+    # ...
 
     # Act
-    copy_file_or_dir(file_mock, "dest")
+    # copy_file_or_dir(myfile, ...)
 
     # Assert
-    assert len(shutil_mock.copy2.call_args_list) == 1
-    assert len(shutil_mock.copytree.call_args_list) == 0
-    copy_call_args = shutil_mock.copy2.call_args_list[0][0]
-
-    actual_file_to_copy = copy_call_args[0]
-    assert actual_file_to_copy.name == 'myfile.txt'
-
-    actual_copy_dest = copy_call_args[1]
-    assert actual_copy_dest == 'dest/myfile.txt'
+    # assert that shutil func was called with correct arguments
+    # for file copying
+    assert False
 
 
-@patch("copiere.copy.shutil")
-def test_copiere_copies_dir(shutil_mock):
+# patch shutil
+def test_copiere_copies_dir():
     # Arrange
-    dir_mock = Mock()
-    dir_mock.is_dir.return_value = True
-    dir_mock.parts = ['path', 'to', 'mydir']
+    # ...
 
     # Act
-    copy_file_or_dir(dir_mock, "dest")
+    # copy_file_or_dir(mydir, ...)
 
     # Assert
-    assert len(shutil_mock.copytree.call_args_list) == 1
-    assert len(shutil_mock.copy2.call_args_list) == 0
-    copy_call_args = shutil_mock.copytree.call_args_list[0]
-
-    assert copy_call_args == call(ANY, "dest/mydir")
+    # assert that shutil func was called with correct arguments
+    # for directory copying
+    assert False
